a49e7f653ef8851867fe08bef51f91e6d879dfb5
lusissy <lourdessissy@gmail.com>
2025-05-20 13:30:00 -0300

    ejercicio1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+bool esNumero;
+string entrada;
+int numero;
+do
+{
+    Console.WriteLine("Ingrese un numero(mayor a 0): ");
+    entrada=Console.ReadLine(); 
+    esNumero=!int.TryParse(entrada, out numero);
+
+        if (!esNumero)
+        {
+            string invertido="";
+            for (int i = entrada.Length-1; i >=0; i--)
+            {
+                invertido+=entrada[i];
+            }
+            Console.WriteLine("El numero invertido es: " +invertido);
+        }else
+        {
+            Console.WriteLine("no es un numero");
+        }
+
+} while (esNumero && numero<0);
+    
